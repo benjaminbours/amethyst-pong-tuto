5054ed78859efcd7e424e932866cbafbaf273045
Benjamin Bours <benjamin.bours@keemotion.com>
2020-02-15 23:25:39 +0100

    Finished part 3 of pong tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+mod pong;
+mod systems;
+
 use amethyst::{
     core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -9,7 +13,6 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-mod pong;
 
 use crate::pong::Pong;
 
@@ -19,6 +22,12 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
+    let mut _world = World::new();
     let game_data = GameDataBuilder::default()
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
@@ -28,7 +37,9 @@ fn main() -> amethyst::Result<()> {
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?
-        .with_bundle(TransformBundle::new())?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"]);
     let assets_dir = app_root.join("assets");
     let mut _world = World::new();
     let mut game = Application::new(assets_dir, Pong, game_data)?;
